8e48f06a7cf2eaa58adf93d15ae978be81b323b1
Oppais2001 <118953310+Oppais2001@users.noreply.github.com>
2023-05-15 22:24:12 -0400

    Update algoritmo_estructurado_DFS.py

--- algoritmo_estructurado_DFS.py
+++ algoritmo_estructurado_DFS.py
@@ -1,6 +1,19 @@
 import networkx as nx
 import random
-def crea_grafo_unidireccional(n,m):#Crea grafo con todas las rutas posibles
+def crea_grafo_unidireccional(n,m):
+    """
+    Crea grafo con todas las rutas posibles
+    (tipo cuadrilla), que es representado
+    como un diccionario.
+    
+    Parametros:
+        n (Entero):cantidad de valores del grafo de alto
+        m (Entero):cantidad de valores del grafo de ancho
+    
+    Retorna:
+        Grafo(Diccionario):almacena todos los nodos con todas
+        las conexiones posibles de nuestro laberinto
+    """
     grafo = nx.DiGraph()
     #Agregar nodos
     for i in range(n):
@@ -26,6 +39,17 @@ def crea_grafo_unidireccional(n,m):#Crea grafo con todas las rutas posibles
     return grafo
 
 def inicio_ruta(n, m):
+    """
+    Toma un valor al azar en el borde del grafo nxm
+    
+    Parametros:
+        n (entero):cantidad de valores del grafo de alto
+        m (entero):cantidad de valores del grafo de ancho
+    
+    Retorna:
+        inicio(entero): una coordenada de dos puntos(x,y)
+        que representa el inicio del laberinto
+    """
     x = random.randint(1,n)
     if x==1:
         y = random.randint(2,m)
@@ -39,6 +63,25 @@ def inicio_ruta(n, m):
     return x,y
     
 def final_ruta(inicio,n, m):
+    """
+    Toma un valor al azar en el borde del
+    grafo nxm, excluyendo el valor del
+    nodo inicial, se encarga de crear una
+    salida rompio el borde donde termine
+    la ruta.
+    
+    Parametros:
+        inicio (Entero coordenadas):dos valores (x,y)
+            que representan al nodo inicial
+        n (Entero):cantidad de valores del grafo de alto
+        m (Entero):cantidad de valores del grafo de ancho
+    
+    Retorna:
+        final (Entero):dos valores de coordenadas para
+            el final del laberinto
+        pared (String): retorna la direccion de la ultima ruta
+            para generar la salida del borde del laberinto
+    """
     x1,y1 = inicio
     x = random.randint(1,n)
     pared=str("")
@@ -63,30 +106,66 @@ def final_ruta(inicio,n, m):
     elif x==n:
         pared="abajo"
         
-    #print("nodo final:", end=" ")
-    #print(x,y)
-    return (x,y),pared
-def dfs(node,node_final,visitados,grafo,):
+    final=(x,y)  
+    return final,pared
+def dfs(node,node_final,visitados,grafo):
+    """
+    Esta función implementa un algoritmo
+    de búsqueda en profundidad
+    (Depth-First Search).
+    (DFS) es un algoritmo de búsqueda no
+    informada que se utiliza para recorrer
+    o buscar elementos en un grafo.
+    La idea principal detrás del DFS es
+    explorar tan profundamente como sea
+    posible en un camino antes de retroceder.
+    
+    Parametros:
+        node(Entero coordenada):Es el nodo actual en el que se encuentra
+            el algoritmo de búsqueda en cada iteración.
+        node_final(Entero coordenada):Es el nodo objetivo que se desea encontrar.
+        visitados(Lista):Es una lista que almacena los nodos
+            visitados durante la ejecución del algoritmo.
+        grafo(Diccionario):Es el grafo representado como un diccionario,
+            que nos muestra todos los nodos y posibles conexiones
+        
+    Retorna:
+        visitados(Lista):lista de visitados que usaremos como ruta en el laberinto
+    """
     visitados.append(node)
     #print("nodo base:", end=" ")
     #print(node)
     for vecinos in grafo[node]:
         if vecinos not in visitados:
-            #print("nodos vecinos:", end=" ")
-            #print(vecinos)
             if vecinos!=node_final:
                 dfs(vecinos,node_final, visitados, grafo)
             else:
                 visitados.append(vecinos)
-                #print("Final")
                 break
             return visitados
-def GrafoaMatriz(n,m,lista,pared):#De grafo a matriz
+def GrafoaMatriz(n,m,lista,pared):
+    """
+    Su Función es tomar la lista que tiene la ruta, y
+    representarla de manera grafica con una matriz.
+    
+    Parametros:
+        n (tipo entero):cantidad de valores del grafo de alto
+        m (tipo entero):cantidad de valores del grafo de ancho
+        lista (lista): lista que almacena la ruta por la que 
+        pasa el laberinto.
+        pared (string):Almacena la ultima direccion de salida
+        para crear la salida del borde. 
+
+    Retorna:
+        matriz(matriz): retorna la matriz que representa la ruta
+        de la lista visitados.
+        indice(entero): numero de valor de largo de la ruta
+    """
     matriz = []
     for i in range((2*n)+1):
         matriz.append([])
         for j in range((2*m)+1):
-            matriz[i].append("X")
+            matriz[i].append("x")
     for a in range(len(lista)):
         x, y = lista[a]
         indice=a
@@ -94,35 +173,42 @@ def GrafoaMatriz(n,m,lista,pared):#De grafo a matriz
         if a+1<len(lista):
             x1,y1 = lista[a+1]
             if x==x1:
-                if y-y1==1:#"i"
-                    matriz[(2*x)-1][(2*y)-2]=" "
-                elif y-y1==-1:#"d"
-                    matriz[(2*x)-1][(2*y)]=" "
+                if y-y1==1:#izquierda
+                    matriz[(2*x)-1][(2*y)-2]=' '
+                elif y-y1==-1:#derecha
+                    matriz[(2*x)-1][(2*y)]=' '
             if y==y1:
-                if x-x1==1:#"s"
-                    matriz[(2*x)-2][(2*y)-1]=" "
-                elif x-x1==-1:#"b"
-                    matriz[(2*x)][(2*y)-1]=" "
+                if x-x1==1:#arriba
+                    matriz[(2*x)-2][(2*y)-1]=' '
+                elif x-x1==-1:#abajo
+                    matriz[(2*x)][(2*y)-1]=' '
         if a==0:
             matriz[(2*x)-1][(2*y)-1]="p"
         if a+1==len(lista):
             if pared=="arriba":
-                matriz[(2*x)-2][(2*y)-1]=" "
+                matriz[(2*x)-2][(2*y)-1]=' '
             if pared=="abajo":
-                matriz[(2*x)][(2*y)-1]=" "
+                matriz[(2*x)][(2*y)-1]=' '
             if pared=="izquierda":
-                matriz[(2*x)-1][(2*y)-2]=" "
+                matriz[(2*x)-1][(2*y)-2]=' '
             if pared=="derecha":
-                matriz[(2*x)-1][(2*y)]=" "
+                matriz[(2*x)-1][(2*y)]=' '
     return matriz,indice
 
-def imprime_matriz(matriz):   
+def imprime_matriz(matriz):
+    """
+    Se encarga de imprimir la matriz en la terminal
+    
+    Parametros:
+        matriz(matriz)=matriz que representa al laberinto
+    """ 
     for lista in matriz:
         print("[", end=" ")
         for elemento in lista:
             print(elemento, end=" ")
         print("]")
-
+        
+#Programa Principal
 n=14
 m=14
 grafo=crea_grafo_unidireccional(n,m)
@@ -131,13 +217,10 @@ final,pared=final_ruta(inicio,n,m)
 lista=[]
 lista=dfs(inicio,final,lista,grafo)
 matriz,i=GrafoaMatriz(n,m,lista,pared)
-while i<195:
+while i<((n*m)-1):
     inicio=inicio_ruta(n,m)
     final,pared=final_ruta(inicio,n,m)
     lista=[]
     lista=dfs(inicio,final,lista,grafo)
     matriz,i=GrafoaMatriz(n,m,lista,pared)
-#print(lista)
-#print(final, pared)
 imprime_matriz(matriz)
-#print(grafo)
